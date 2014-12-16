0dba22a244cdfb3c4b73dbed08aa03c722361791
Aaditi Jain <aaditi2290@gmail.com>
2014-12-16 20:51:57 +0530

    Added Answers filter

--- lib/so_repute.rb
+++ lib/so_repute.rb
@@ -4,14 +4,12 @@ module SoRepute
   class Base
   	def initialize(user_id, app_key=nil)
       user_info = HTTParty.get("https://api.stackexchange.com/users/#{user_id.to_s}/?site=stackoverflow" + (app_key.nil? ? "" : "&key=#{app_key}")).parsed_response
-      user_answers = HTTParty.get("https://api.stackexchange.com/users/#{user_id.to_s}/answers/?site=stackoverflow" + (app_key.nil? ? "" : "&key=#{app_key}")).parsed_response
-      user_questions = HTTParty.get("https://api.stackexchange.com/users/#{user_id.to_s}/questions/?site=stackoverflow" + (app_key.nil? ? "" : "&key=#{app_key}")).parsed_response
       if user_info.keys.include?("error_id")
       	raise "Incorrect user_id or app_key"
       else
       	@user_info = user_info["items"][0]
-      	@user_answers = user_answers["items"]
-      	@user_questions = user_questions["items"]
+      	@user_answers = HTTParty.get("https://api.stackexchange.com/users/#{user_id.to_s}/answers/?site=stackoverflow&pagesize=100&filter=!9YdnSQVoS" + (app_key.nil? ? "" : "&key=#{app_key}")).parsed_response
+      	@user_questions = user_questions = HTTParty.get("https://api.stackexchange.com/users/#{user_id.to_s}/questions/?site=stackoverflow" + (app_key.nil? ? "" : "&key=#{app_key}")).parsed_response
       end
   	end
 
@@ -34,9 +32,13 @@ module SoRepute
   	def reputation_change_day
   	  @user_info["reputation_change_day"]
   	end
+
+    def badges
+     {bronze: bronze, silver: silver, gold: gold }      
+    end
     
     def total_answers
-      @user_answers.length
+      total = @user_answers[:total]
     end 
   end
 end
