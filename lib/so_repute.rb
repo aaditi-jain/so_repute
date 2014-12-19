1932c8ca284214b3e1b1106beab056f06ffc8b57
Aaditi Jain <aaditi2290@gmail.com>
2014-12-19 19:46:03 +0530

    Removed extra instance variables

--- lib/so_repute.rb
+++ lib/so_repute.rb
@@ -11,9 +11,7 @@ module SoRepute
       if user_info.keys.include?("error_id")
       	raise ((user_info["error_name"] == "throttle_violation") ? "Number Of Requests exceeded the daily quota of #{@app_key.nil? ? 300 : 10000}" : "Incorrect user_id or app_key")
       else
-      	@user_info = user_info["items"][0]
-      	@user_answers = HTTParty.get("https://api.stackexchange.com/users/#{@user_id}/answers/?site=stackoverflow&pagesize=100&filter=!9YdnSQVoS&page=1" + (@app_key.nil? ? "" : "&key=#{@app_key}")).parsed_response
-      	@user_questions = HTTParty.get("https://api.stackexchange.com/users/#{@user_id}/questions/?site=stackoverflow&filter=!9YdnSQVoS&pagesize=100&page=1" + (@app_key.nil? ? "" : "&key=#{@app_key}")).parsed_response
+      	@user_info = user_info["items"][0]      
       end
   	end
 
@@ -46,16 +44,26 @@ module SoRepute
     end
     
     def total_answers
-      @user_answers["total"]
+      user_answers = HTTParty.get("https://api.stackexchange.com/users/#{@user_id}/answers/?site=stackoverflow&pagesize=100&filter=!9YdnSQVoS&page=1" + (@app_key.nil? ? "" : "&key=#{@app_key}")).parsed_response      
+      if user_answers.keys.include?("error_id")
+        raise ((user_answers["error_name"] == "throttle_violation") ? "Number Of Requests exceeded the daily quota of #{@app_key.nil? ? 300 : 10000}" : "Error occured while fetching data. ")
+      else
+        user_answers["total"]   
+      end      
     end 
 
     def accepted_answers   
-      @search_page = Nokogiri::HTML(open("https://stackoverflow.com/search?q=user%3A#{@user_id}+isaccepted%3Ayes"))  
-      @search_page.css('div.results-header > h2').first.text.delete(",results").strip.to_i
+      search_page = Nokogiri::HTML(open("https://stackoverflow.com/search?q=user%3A#{@user_id}+isaccepted%3Ayes"))  
+      search_page.css('div.results-header > h2').first.text.delete(",results").strip.to_i
     end
 
     def total_questions
-      @user_questions["total"]
+      user_questions = HTTParty.get("https://api.stackexchange.com/users/#{@user_id}/questions/?site=stackoverflow&filter=!9YdnSQVoS&pagesize=100&page=1" + (@app_key.nil? ? "" : "&key=#{@app_key}")).parsed_response
+      if user_questions.keys.include?("error_id")
+        raise ((user_questions["error_name"] == "throttle_violation") ? "Number Of Requests exceeded the daily quota of #{@app_key.nil? ? 300 : 10000}" : "Error occured while fetching data")
+      else
+        user_questions["total"]   
+      end   
     end
 
   end
